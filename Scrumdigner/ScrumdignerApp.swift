85826831c9e88fe4b4ad6820a3ef9f88bcd3cc0f
Marco Espinosa <marco@tinkin.one>
2021-06-08 00:44:32 -0500

    Initial Commit

---
+++ Scrumdigner/ScrumdignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdignerApp.swift
+//  Scrumdigner
+//
+//  Created by Marco Espinosa on 8/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
